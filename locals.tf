8d958e37fbdc9c68d2f6b2be042e9fd49ad6ab94
Muraliprasad-M <MuraliPrasad.M@cognizant.com>
2026-03-06 10:15:59 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -12,3 +12,13 @@ locals {
 
   cgw_ips = var.cgw_public_ips
 }
+
+# locals {
+#   name_prefix = "${var.domain}-${var.env}"
+#   cgw_ips     = var.cgw_public_ips
+#   common_tags = merge(var.tags, {
+#     Environment = var.env,
+#     Domain      = var.domain,
+#     ManagedBy   = "terraform"
+#   })
+# }
